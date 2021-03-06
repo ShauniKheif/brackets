6c180dc582cf2bba55ee268ddd540ba69117381c
ShauniKheif <53867436+ShauniKheif@users.noreply.github.com>
2021-03-06 18:53:35 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  str = str.split('');
+  let stack = [];
+  if (str.length % 2) return false;
+
+  for (let a = 0; a < str.length; a++) {
+    for (let i=0; i < bracketsConfig.length; i++) {
+      if  ((str[a] == bracketsConfig[i][1]) && (stack[stack.length-1] == bracketsConfig[i][0])) {
+        stack.pop();
+        continue;
+      }
+      
+      if  (str[a] == bracketsConfig[i][0]) {
+        stack.push(str[a]);
+      } 
+    }
+  }
+  return stack.length == 0 ? true : false; 
+ }
 }
